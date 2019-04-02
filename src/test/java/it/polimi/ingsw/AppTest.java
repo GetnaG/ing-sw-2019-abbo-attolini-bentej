c6f7a68e5d94c7f4fb949501d40cb01d9a67f528
Fahed Ben Tej <fahed.ben@mail.polimi.it>
2019-04-02 16:06:07 +0200

    Tests are now JUnit 5 compliant

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,20 +1,19 @@
 package it.polimi.ingsw;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertEquals(2, 1 + 1);
     }
 }
