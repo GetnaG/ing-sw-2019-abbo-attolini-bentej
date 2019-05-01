95d8e127bd8f933994d54f08398a67858b03354b
Fahed Ben Tej <fahed.ben@mail.polimi.it>
2019-05-01 19:31:55 +0200

    Enhancement of GameBoard class.

--- src/main/java/it/polimi/ingsw/server/model/board/GameBoard.java
+++ src/main/java/it/polimi/ingsw/server/model/board/GameBoard.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 /**
  * Represents the Game Board used in the game. It contains all the information about the configuration of the rooms, the decks used and the mode in which the Game is played.
+ *
  * @author Fahed Ben Tej
  */
 public class GameBoard implements ReplaceListener {
@@ -63,7 +64,8 @@ public class GameBoard implements ReplaceListener {
 
     /**
      * Construct a GameBoard using the {@code track} which defines the mode and the {@code configuration} of the rooms .
-     * @param track defines the mode in which the game will be played. {@see KillshotTrack} {@see DominationTrack} {@see TurretTrack}
+     *
+     * @param track         defines the mode in which the game will be played. {@see KillshotTrack} {@see DominationTrack} {@see TurretTrack}
      * @param configuration defines the rooms which compose the boards. Note that rooms are composed squares.
      */
     public GameBoard(AbstractTrack track, List<Room> configuration) {
@@ -92,6 +94,7 @@ public class GameBoard implements ReplaceListener {
 
     /**
      * Gets a random Ammo Card from the Ammo Deck. Note that Ammo Card can always be drawn. {@see AmmoDeck}
+     *
      * @return random Ammo Card drawn from the Ammo Deck.
      */
     public AmmoCard getAmmoCard() {
@@ -100,6 +103,7 @@ public class GameBoard implements ReplaceListener {
 
     /**
      * Gets a random Powerup Card from the Powerup Deck. Note that Powerup Card can always be drawn {@see PowerupDeck}.
+     *
      * @return random Powerup Card from the Powerup Deck.
      */
     public PowerupCard getPowerupCard() {
@@ -108,8 +112,8 @@ public class GameBoard implements ReplaceListener {
 
     /**
      * Gets a random Weapon Card from the Weapon Deck. Note that Weapon Cards can't be drawn if the Deck is empty {@see WeaponDeck}.
-     * @return random Weapon Card from the Weapon Deck.
      *
+     * @return random Weapon Card from the Weapon Deck.
      */
     public WeaponCard getWeaponCard() throws AgainstRulesException {
         return weaponDeck.drawCard();
@@ -117,11 +121,12 @@ public class GameBoard implements ReplaceListener {
 
     /**
      * Finds the {@code Square} which contains the {@code Spawn} of the given {@code Color}
+     *
      * @param color the color of the spawn to find
      * @return a {@code Square} which contains the {@code Spawn} of the given {@code Color}
      */
     public Square findSpawn(AmmoCube color) {
-        List<SpawnSquare> spawns =  configuration.stream()
+        List<SpawnSquare> spawns = configuration.stream()
                 .filter(Room::hasSpawnSquare).map(x -> x.getSpawnSquare()).filter(x -> x.getSpawnColor() == color).collect(Collectors.toList());
 
         return spawns.get(0);
@@ -129,8 +134,9 @@ public class GameBoard implements ReplaceListener {
 
     /**
      * Gets all the valid squares in which a player can move. The {@code sameDirection} flag is used to specify the constraint of moving in only one direction.
-     * @param start  starting square
-     * @param maxDistance  maximum moves square-to-square permitted
+     *
+     * @param start         starting square
+     * @param maxDistance   maximum moves square-to-square permitted
      * @param sameDirection flag used to specify the constraint of moving in only one direction.
      * @return all the squares in which a player can move. The {@code start} square is excluded.
      */
@@ -142,50 +148,49 @@ public class GameBoard implements ReplaceListener {
             return validDestinations;
 
 
-        if(sameDirection) {
+        if (sameDirection) {
             Square curSquare = start;
             int curDistance = maxDistance;
             // North direction Squares
-            while (curSquare.getNorthBorder() != Border.WALL  && curDistance>0){
+            while (curSquare.getNorthBorder() != Border.WALL && curDistance > 0) {
                 validDestinations.add(curSquare.getNorth());
                 curSquare = curSquare.getNorth();
-                curDistance = curDistance -1;
+                curDistance = curDistance - 1;
             }
             curSquare = start;
             curDistance = maxDistance;
             // East direction Squares
-            while (curSquare.getEastBorder() != Border.WALL && curDistance>0){
+            while (curSquare.getEastBorder() != Border.WALL && curDistance > 0) {
                 validDestinations.add(curSquare.getEast());
                 curSquare = curSquare.getEast();
-                curDistance = curDistance -1;
+                curDistance = curDistance - 1;
             }
             curSquare = start;
             curDistance = maxDistance;
             // South direction Squares
-            while (curSquare.getSouthBorder() != Border.WALL  && curDistance>0){
+            while (curSquare.getSouthBorder() != Border.WALL && curDistance > 0) {
                 validDestinations.add(curSquare.getSouth());
                 curSquare = curSquare.getSouth();
-                curDistance = curDistance -1;
+                curDistance = curDistance - 1;
             }
             curSquare = start;
             curDistance = maxDistance;
             // West direction Squares
-            while (curSquare.getWestBorder() != Border.WALL  && curDistance>0){
+            while (curSquare.getWestBorder() != Border.WALL && curDistance > 0) {
                 validDestinations.add(curSquare.getWest());
                 curSquare = curSquare.getWest();
-                curDistance = curDistance -1;
+                curDistance = curDistance - 1;
             }
 
             return validDestinations;
         } else {
             // Get all squares in the radius defined by maxDistance
-            radiusValidDestinations(start,maxDistance,validDestinations);
+            radiusValidDestinations(start, maxDistance, validDestinations);
             validDestinations.remove(start);
 
             return validDestinations;
 
         }
-
 
 
     }
@@ -194,56 +199,56 @@ public class GameBoard implements ReplaceListener {
      * Helper method for {@link GameBoard#getValidDestinations(Square, int, boolean)}.
      * This method uses recursion to find the valid destinations.
      */
-    private void radiusValidDestinations(Square start, int maxDistance, List<Square> alreadyVisitedSquares){
+    private void radiusValidDestinations(Square start, int maxDistance, List<Square> alreadyVisitedSquares) {
 
-        if(maxDistance == 0) return;
+        if (maxDistance == 0) return;
 
-        if(start.getNorthBorder() != Border.WALL && maxDistance>0 ){
+        if (start.getNorthBorder() != Border.WALL && maxDistance > 0) {
             if (alreadyVisitedSquares.contains(start.getNorth()))
                 return;
             alreadyVisitedSquares.add(start.getNorth());
-            radiusValidDestinations(start.getNorth(), maxDistance-1, alreadyVisitedSquares);
+            radiusValidDestinations(start.getNorth(), maxDistance - 1, alreadyVisitedSquares);
         }
-        if(start.getEastBorder() != Border.WALL && maxDistance>0){
+        if (start.getEastBorder() != Border.WALL && maxDistance > 0) {
             if (alreadyVisitedSquares.contains(start.getEast()))
                 return;
             alreadyVisitedSquares.add(start.getEast());
-            radiusValidDestinations(start.getEast(), maxDistance-1, alreadyVisitedSquares);
+            radiusValidDestinations(start.getEast(), maxDistance - 1, alreadyVisitedSquares);
         }
-        if(start.getSouthBorder() != Border.WALL && maxDistance>0){
+        if (start.getSouthBorder() != Border.WALL && maxDistance > 0) {
             if (alreadyVisitedSquares.contains(start.getSouth()))
                 return;
             alreadyVisitedSquares.add(start.getSouth());
-            radiusValidDestinations(start.getSouth(), maxDistance-1, alreadyVisitedSquares);
+            radiusValidDestinations(start.getSouth(), maxDistance - 1, alreadyVisitedSquares);
         }
-        if(start.getWestBorder() != Border.WALL && maxDistance>0){
+        if (start.getWestBorder() != Border.WALL && maxDistance > 0) {
             if (alreadyVisitedSquares.contains(start.getWest()))
                 return;
             alreadyVisitedSquares.add(start.getWest());
-            radiusValidDestinations(start.getWest(), maxDistance-1, alreadyVisitedSquares);
+            radiusValidDestinations(start.getWest(), maxDistance - 1, alreadyVisitedSquares);
         }
 
     }
 
     /**
      * Used to insert tokens in the Track.
-     * @param tokens the player who did the kill. If there's an overkill, {@code tokens} must contain two times the same player.
-     *              In case of 3+ kills, they are counted as 2. If list {@code tokens} is empty, no kill is counted.
      *
+     * @param tokens the player who did the kill. If there's an overkill, {@code tokens} must contain two times the same player.
+     *               In case of 3+ kills, they are counted as 2. If list {@code tokens} is empty, no kill is counted.
      */
-    public void addTokensAndRemoveSkull (List<Player> tokens) {
+    public void addTokensAndRemoveSkull(List<Player> tokens) {
         track.removeSkull();
         track.addTokens(tokens);
     }
 
     /**
      * Checks whether Final Frenzy is triggered.
+     *
      * @return boolean indicating whether Final Frenzy is triggered.
      */
     public boolean checkFinalFrenzy() {
         return track.getSkullsLeft() == 0;
     }
-
 
 
     /**
@@ -255,6 +260,7 @@ public class GameBoard implements ReplaceListener {
 
     /**
      * Inserts an Ammo Card in the {@code discardedAmmos} set.
+     *
      * @param usedCard the discarded Ammo Card.
      */
     public void putAmmoCard(AmmoCard usedCard) {
@@ -264,6 +270,7 @@ public class GameBoard implements ReplaceListener {
 
     /**
      * Inserts an Powerup Card in the {@code discardedPowerups} set.
+     *
      * @param usedCard the discarded Powerup Card.
      */
     public void putPowerupCard(PowerupCard usedCard) {
@@ -273,8 +280,9 @@ public class GameBoard implements ReplaceListener {
     /**
      * When an Ammo Card is grabbed from a Square, it is replaced  at the end of the turn.
      * This method stores the squares where there is an Ammo Card to be replaced.
+     *
      * @param toBeReplaced the square where there is an Ammo Card to be replaced.
-     * The method {@link GameBoard#replaceAll} actually takes care of replacing the Ammo Card at the end of turn.
+     *                     The method {@link GameBoard#replaceAll} actually takes care of replacing the Ammo Card at the end of turn.
      */
     public void addTurretSquare(TurretSquare toBeReplaced) {
         squareNewAmmoCard.add(toBeReplaced);
@@ -283,8 +291,9 @@ public class GameBoard implements ReplaceListener {
     /**
      * When a Weapon Card is grabbed from a Spawn Square, it is replaced at the end of the turn.
      * This method stores the Spawn Squares where there is a Weapon Card to be replaced.
+     *
      * @param toBeReplaced SpawnSquare where there's a Weapon to be replaced.
-     * The method {@link GameBoard#replaceAll} actually takes care of replacing the Weapon Card at the end of turn.
+     *                     The method {@link GameBoard#replaceAll} actually takes care of replacing the Weapon Card at the end of turn.
      */
     public void addSpawnSquare(SpawnSquare toBeReplaced) {
         squareNewWeaponCard.add(toBeReplaced);
@@ -304,17 +313,17 @@ public class GameBoard implements ReplaceListener {
     public void replaceAll() {
 
         //Replacing Ammo Cards
-        for(Square s : squareNewAmmoCard)
+        for (Square s : squareNewAmmoCard)
             s.setGrabbable(ammoDeck.drawCard());
         //Replacing Weapon Card
         for (SpawnSquare s : squareNewWeaponCard)
-            if (weaponDeck.cardsLeft() !=0){
-                for(WeaponCard i: s.getMarket().getCards()) {   //drawWeaponCard solo finchè ci sono null, controllo in testa
+            if (weaponDeck.cardsLeft() != 0) {
+                for (WeaponCard i : s.getMarket().getCards()) {   //drawWeaponCard solo finchè ci sono null, controllo in testa
 
                     try {
                         s.getMarket().addCard(weaponDeck.drawCard());
                     } catch (AgainstRulesException e) {
-                         }
+                    }
                 }
             }
 
@@ -322,6 +331,7 @@ public class GameBoard implements ReplaceListener {
 
     /**
      * Gets discarded powerups.
+     *
      * @return discarded powerups
      */
     public List<PowerupCard> getDiscardedPowerups() {
@@ -330,12 +340,107 @@ public class GameBoard implements ReplaceListener {
 
     /**
      * Gets discarded ammos.
+     *
      * @return discarded ammos
      */
     public List<AmmoCard> getDiscardedAmmos() {
         return discardedAmmos;
     }
 
+    /**
+     * Gets the minimum distance between two squares, ignoring the wall according to the {@code wallSensitive} parameter.
+     *
+     * @param wallSensitive boolean indicating if the path should ignore walls or not
+     * @return an int representing the minimum distance between two squares.
+     */
+    public int minimumDistance(Square start, Square end, Boolean wallSensitive) {
+        if (start.equals(end))
+            return 0;
+
+        Set<Square> Q = getAllSquares();
+        Map<Square, Integer> dist = new HashMap<>();
+        Map<Square, Square> prev = new HashMap<>();
+
+        Q.forEach(x -> {
+            dist.put(x, 999);
+            prev.put(x, null);
+        });
+        dist.put(start, 0);
+        Square u = start;
+        while (!Q.isEmpty()){
+            u = min(Q, dist);
+            Q.remove(u);
+            List<Square> neighbours = getNeighbours(u, wallSensitive);
+            for (Square v : neighbours){
+                int distance = dist.get(u) + 1;
+                if (distance < dist.get(v))
+                    dist.replace(v, distance);
+            }
+        }
+        return dist.get(end);
+    }
+
+    /**
+     * Gets the square  with the minimum distance in the given set of squares.
+     * @param squares set of squares
+     * @param map     distance for every square
+     * @return
+     */
+    private Square min(Set<Square> squares, Map<Square,Integer> map){
+        int min = 999;
+        Square minSquare  = null;
+        for (Square s: squares)
+            if (map.get(s)< min){
+                min = map.get(s);
+                minSquare = s;
+            }
+        return  minSquare;
 
 
+
+    }
+
+    /**
+     * Gets the walkable neighbours of a square.
+     *
+     * @param s square
+     * @return
+     */
+    private List<Square> getNeighbours(Square s, Boolean wallSensitive) {
+        List<Square> squares = new ArrayList<>();
+
+        if (s.getNorthBorder() != Border.WALL || !wallSensitive)
+            squares.add(s.getNorth());
+        if (s.getEastBorder() != Border.WALL || !wallSensitive)
+            squares.add(s.getEast());
+        if (s.getSouthBorder() != Border.WALL || !wallSensitive)
+            squares.add(s.getSouth());
+        if (s.getWestBorder() != Border.WALL || !wallSensitive)
+            squares.add(s.getWest());
+
+        return squares;
+    }
+
+
+    /**
+     * Gets all the players in the given square.
+     *
+     * @param square
+     * @return a set presenting the players in the given square
+     */
+    public Collection<Player> getPlayerInSquare(Square square, Collection<Player> players) {
+        return players.stream().filter(p -> p.getPosition().equals(square)).collect(Collectors.toSet());
+    }
+
+    /**
+     * Gets all the square composing the board.
+     *
+     * @return all squares composing the board
+     */
+    public Set<Square> getAllSquares() {
+        return configuration.stream()
+                .map(room -> room.getSquares()).
+                        flatMap(List::stream)
+                .collect(Collectors.toSet());
+    }
 }
--- src/test/java/it/polimi/ingsw/server/model/board/GameBoardTest.java
+++ src/test/java/it/polimi/ingsw/server/model/board/GameBoardTest.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.server.model.board;
 
+import it.polimi.ingsw.server.model.player.Player;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,17 +13,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameBoardTest {
     static private GameBoard gm;
+    static private GameBoard gm2;
     static private List<Room> configuration;
+    static private List<Room> configuration2;
     static private SpawnSquare firstSquare;
     static private Square secondSquare;
     static private Square thirdSquare;
     static private Square fourthSquare;
+    static private Square fifthSquare;
+    static private Square sixthSquare;
+    static private Square seventhSquare;
 
 
     @BeforeAll
     static void setUp(){
         //Creating a simple configuration
         List<Square> squares = new ArrayList<>();
+        List<Square> squares2 = new ArrayList<>();
 
         // Adding rooms and doors.
         firstSquare = new SpawnSquare(Color.GREEN,new WeaponMarket(new ArrayList<>()));
@@ -84,7 +91,86 @@ class GameBoardTest {
         configuration.add(secondRoom);
         gm = new GameBoard(new KillshotTrack(), configuration);
 
+        // Second configuration
+        fifthSquare = new Square(Color.BLUE);
+        sixthSquare = new Square(Color.GREEN);
+        seventhSquare = new Square(Color.BLUE);
 
+        squares2.addAll(squares);
+        squares2.add(fifthSquare);
+        squares2.add(sixthSquare);
+        squares2.add(seventhSquare);
+
+        // By default they are all isolated
+        squares2.forEach(x -> x.setNorthBorder(Border.WALL));
+        squares2.forEach(x -> x.setEastBorder(Border.WALL));
+        squares2.forEach(x -> x.setSouthBorder(Border.WALL));
+        squares2.forEach(x -> x.setWestBorder(Border.WALL));
+        // Set connections with other squares
+
+        firstSquare.setSouth(thirdSquare);
+        firstSquare.setSouthBorder(Border.CORRIDOR);
+        firstSquare.setEast(secondSquare);
+        firstSquare.setEastBorder(Border.DOOR);
+
+        secondSquare.setWest(firstSquare);
+        secondSquare.setWestBorder(Border.DOOR);
+        secondSquare.setSouthBorder(Border.CORRIDOR);
+        secondSquare.setSouth(fourthSquare);
+
+        thirdSquare.setNorth(firstSquare);
+        thirdSquare.setNorthBorder(Border.CORRIDOR);
+        thirdSquare.setEast(fourthSquare);
+        thirdSquare.setEastBorder(Border.WALL);
+
+        fourthSquare.setNorth(secondSquare);
+        fourthSquare.setNorthBorder(Border.CORRIDOR);
+        fourthSquare.setWest(thirdSquare);
+        fourthSquare.setWestBorder(Border.WALL);
+
+        fifthSquare.setSouth(sixthSquare);
+        fifthSquare.setSouthBorder(Border.CORRIDOR);
+        fifthSquare.setWest(fourthSquare);
+        fifthSquare.setWestBorder(Border.DOOR);
+
+        sixthSquare.setNorth(fifthSquare);
+        sixthSquare.setNorthBorder(Border.CORRIDOR);
+        sixthSquare.setEast(seventhSquare);
+        sixthSquare.setEastBorder(Border.DOOR);
+
+        seventhSquare.setWest(sixthSquare);
+        seventhSquare.setWestBorder(Border.CORRIDOR);
+
+        List<Square> room3 = new ArrayList<>();
+        room3.add(fifthSquare);
+        room3.add(sixthSquare);
+        room3.add(seventhSquare);
+        Room thirdRoom = new Room(room3);
+
+        squaresInRoom = new ArrayList<>();
+        squaresInRoom.add(secondSquare);
+        squaresInRoom.add(fourthSquare);
+        Room secondRoom2 = new Room(squaresInRoom);
+
+        fifthSquare.setRoom(thirdRoom);
+        sixthSquare.setRoom(thirdRoom);
+        seventhSquare.setRoom(thirdRoom);
+
+        configuration2 = new ArrayList<>();
+        configuration2.add(firstRoom);
+        configuration2.add(secondRoom);
+        configuration2.add(thirdRoom);
+        gm2 = new GameBoard(new KillshotTrack(), configuration2);
+
+        /*
+        First configuration looks like :
+                                            1   <>   2
+                                            3   |    4
+        First configuration looks like :
+                                            1   <>   2
+                                            3    |   4    5
+                                                          6   7
+         */
 
     }
     @Test
@@ -122,6 +208,16 @@ class GameBoardTest {
 
     }
 
+    @Test
+    void minimumDistance() {
+        fourthSquare.setEastBorder(Border.DOOR);
+        fourthSquare.setEast(fifthSquare);
+        assertEquals(0, gm2.minimumDistance(firstSquare, firstSquare, true));
+        assertEquals(0, gm2.minimumDistance(firstSquare, firstSquare, false));
+
+        assertEquals(2, gm2.minimumDistance(firstSquare, fourthSquare, true));
+        assertEquals(4, gm2.minimumDistance(secondSquare, seventhSquare , true));
+    }
 
     @Test
     void replaceAll() {
@@ -192,6 +288,30 @@ class GameBoardTest {
         conf.add(firstRoom);
         conf.add(secondRoom);
         return new GameBoard(new KillshotTrack(), conf);
+
+    }
+
+    @Test
+    void getPlayerInSquare() {
+        Player p1 = new Player("A");
+        p1.setPosition(firstSquare);
+        Player p2 = new Player("B");
+        p2.setPosition(firstSquare);
+        Player p3 = new Player("C");
+        p3.setPosition(firstSquare);
+        Player p4 = new Player("D");
+        p4.setPosition(secondSquare);
+        List<Player> players = new ArrayList<>();
+        List<Player> allPlayers = new ArrayList<>();
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+        allPlayers.addAll(players);
+        allPlayers.add(p4);
+
+        assertTrue(players.containsAll(gm.getPlayerInSquare(firstSquare, allPlayers)));
+        assertTrue(!players.contains(gm.getPlayerInSquare(thirdSquare, allPlayers)));
+
 
     }
 }
