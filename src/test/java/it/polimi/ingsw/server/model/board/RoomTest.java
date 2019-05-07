c9a6e870e68956c2e16a60585e5cad222be58307
SilvioAttolini <silvio.attolini@mail.polimi.it>
2019-05-07 17:12:13 +0200

    room tests and square ID

---
+++ src/test/java/it/polimi/ingsw/server/model/board/RoomTest.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.server.model.board;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @Test
+    void hasSpawnSquare() {
+        /*Square a = new Square(Color.YELLOW), b = new Square(Color.BLUE), c = new Square(Color.WHITE);
+        List<Square> l = new ArrayList<>();
+        l.add(a);
+        l.add(b);
+        l.add(c);
+        Room r = new Room(l);
+
+        assertTrue(!r.hasSpawnSquare());*/
+    }
+
+    @Test
+    void equals() {
+    }
+}
