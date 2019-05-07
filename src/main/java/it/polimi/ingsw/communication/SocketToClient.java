269a516d8d063040484f4084acea1877c632c6fa
Giulio Antonio Abbo <giulioantonio.abbo@mail.polimi.it>
2019-05-07 13:10:09 +0200

    Created empty classes for communication and server logic.

---
+++ src/main/java/it/polimi/ingsw/communication/SocketToClient.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.communication;
+
+/**
+ * Descrizione.
+ * <p>
+ * Dettagli.
+ *
+ * @author Abbo Giulio A.
+ */
+public class SocketToClient {
+}
---
+++ src/main/java/it/polimi/ingsw/server/serverlogic/Nicknames.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.server.serverlogic;
+
+/**
+ * Descrizione.
+ * <p>
+ * Dettagli.
+ *
+ * @author Abbo Giulio A.
+ */
+public class Nicknames {
+}
