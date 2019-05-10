dfe41e27b74586689df17557f6c916eee0921d23
poligiulio <giulioantonio.abbo@mail.polimi.it>
2019-05-10 20:23:58 +0200

    Tested SocketProtocol and ToClientException.

---
+++ src/test/java/it/polimi/ingsw/communication/SocketProtocolTest.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.communication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+ * Author: Abbo Giulio A.
+ */
+class SocketProtocolTest {
+
+    /*Testing that the right element is returned*/
+    @Test
+    void with() {
+        for (SocketProtocol element : SocketProtocol.values())
+            assertEquals(element, SocketProtocol.with(element.getCommand()));
+    }
+
+    /*Testing that getCommand returns the same as toString*/
+    @Test
+    void getCommand() {
+        for (SocketProtocol element : SocketProtocol.values())
+            assertEquals(element.toString(), element.getCommand());
+    }
+
+    /*Testing that toStrings returns the command*/
+    @Test
+    void toString1() {
+        for (SocketProtocol element : SocketProtocol.values())
+            assertEquals(element.getCommand(), element.toString());
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/communication/ToClientExceptionTest.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.communication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/*
+ * Author: Abbo Giulio A.
+ * Testing: correct behaviour of the constructors.
+ */
+class ToClientExceptionTest {
+    private static final String MESSAGE = "Test message";
+
+    /*Testing constructor with only message*/
+    @Test
+    void constructor_message() {
+        ToClientException e = new ToClientException("test message");
+
+        assertTrue(("Communication exception, " + MESSAGE).equalsIgnoreCase(e.getMessage()));
+    }
+
+    /*Testing constructor with message and cause*/
+    @Test
+    void constructor_cause() {
+        Exception cause = new Exception();
+        ToClientException e = new ToClientException("test message", cause);
+
+        assertTrue(("Communication exception, " + MESSAGE).equalsIgnoreCase(e.getMessage()));
+        assertEquals(cause, e.getCause());
+    }
+}
