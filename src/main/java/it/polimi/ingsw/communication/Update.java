cb3454e8041c49ac60455920952ced3875295428
Giulio Antonio Abbo <giulioantonio.abbo@mail.polimi.it>
2019-05-21 14:59:48 +0200

    Updated Update class.

--- src/main/java/it/polimi/ingsw/communication/Update.java
+++ src/main/java/it/polimi/ingsw/communication/Update.java
@@ -15,11 +15,32 @@ class Update {
     /**
      * The new value for the field affected.
      */
-    String newValue;
+    String[] newValue;
+    /**
+     * Identifies the receiver of the update.
+     */
+    String nickname;
 
     /**
      * The available field that can be modified.
      */
     enum UpdateType {
+        CONFIGURATION_ID,
+        AMMOCARD_ARRAY,
+        WEAPONCARD_ARRAY,
+        IS_WEAPON_DECK_DRAWABLE,
+        KILLSHOT_TRACK,//with separator
+        IS_ACTION_TILE_FRENZY,
+        TURN_POSITION,
+        SQUARE_POSITION,
+        NICKNAME,
+        AMMOCUBE_ARRAY,
+        IS_PLAYER_BOARD_FRENZY,
+        SKULL_NUMBER,
+        DAMAGE_ARRAY,
+        IS_CONNECTED,
+        LOADED_WEAPONS,
+        UNLOADED_WEAPON,
+        POWERUPS
     }
 }
